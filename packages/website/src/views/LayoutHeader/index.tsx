202459e29a9d4caaeb7e95dbd9bc8f965200cafc
shlroland <shlroland1995@gmail.com>
2021-01-17 16:53:11 +0800

    完成初步header布局

--- packages/website/src/views/LayoutHeader/index.tsx
+++ packages/website/src/views/LayoutHeader/index.tsx
@@ -4,8 +4,18 @@ import styles from '@/views/LayoutHeader/LayoutHeader.module.css'
 const LayoutHeader: FC = () => {
   return (
     <header className={styles.wrapper}>
-      <div className={styles.logo}></div>
-      <div className={styles.content}></div>
+      <div className={styles.logo}>
+        <a className="overflow-hidden w-auto">
+          <img className="w-10 h-10" src={require('@/assets/images/logo.png')} alt="/" />
+        </a>
+      </div>
+      <nav className={styles.content}>
+        <ul className={styles['content-nav']}>
+          <li className="h-full inline-flex items-center pr-8">首页</li>
+          <li className="h-full inline-flex items-center pr-8">归档</li>
+        </ul>
+        <ul className={styles['content-action']}></ul>
+      </nav>
     </header>
   )
 }
