deb10adc211c3af916bdfa0a3c4b9a70348cbc66
shlroland <shlroland1995@gmail.com>
2021-05-18 14:52:09 +0800

    fix: 调整了首页和article card的布局

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import styles from '@/styles/App.module.css'
 import LayoutContainer from '@/views/LayoutContainer/LayoutContainer'
 import ArticleList from '@/components/ArticleList/index'
+import ListBox from '@/components/ListBox'
 
 export default function Home() {
   return (
     <>
       <main className={styles.wrapper}>
-        <LayoutContainer>
-          <ArticleList></ArticleList>
+        <LayoutContainer asideCom={ListBox()}>
+          <ArticleList />
         </LayoutContainer>
       </main>
     </>
