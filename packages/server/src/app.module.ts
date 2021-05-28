25950905bca6cf98ab0f52d7b75da2a6001432f7
shlroland <shlroland1995@gmail.com>
2021-05-29 00:07:46 +0800

    feat(server): 添加monogo数据库

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -4,8 +4,15 @@ import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from './config/config.module'
 import { GraphqlModule } from './graphql/graphql.module'
 import { PostsModule } from './posts/posts.module'
+import { DatabaseModule } from './database/database.module'
 @Module({
-  imports: [AuthModule, ConfigModule, GraphqlModule, PostsModule],
+  imports: [
+    AuthModule,
+    ConfigModule,
+    GraphqlModule,
+    PostsModule,
+    DatabaseModule,
+  ],
   providers: [AuthService],
 })
 export class AppModule {}
