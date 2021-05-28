47138cd23e49451f749ce416b07cef1315c91f73
shlroland <shlroland1995@gmail.com>
2021-05-28 18:48:49 +0800

    feat(server): 添加基础的gql服务

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth/auth.service'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from './config/config.module'
-
+import { GraphqlModule } from './graphql/graphql.module'
+import { PostsModule } from './posts/posts.module'
 @Module({
-  imports: [AuthModule, ConfigModule],
+  imports: [AuthModule, ConfigModule, GraphqlModule, PostsModule],
   providers: [AuthService],
 })
 export class AppModule {}
--- packages/server/src/graphql/graphql.module.ts
+++ packages/server/src/graphql/graphql.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
-import { ConfigModule } from '@nestjs/config'
+import { join } from 'path'
 
 @Module({
-  imports: [ConfigModule, GraphQLModule.forRootAsync({})],
+  imports: [
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      async useFactory(...rest) {
+        console.log(rest, join(process.cwd(), 'src/schema.gql'))
+        return {
+          // typePaths: ['./**/*.gql'],
+          context: ({ req, res }) => ({ req, res }),
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        }
+      },
+    }),
+  ],
 })
 export class GraphqlModule {}
