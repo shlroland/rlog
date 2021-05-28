49cdf76218aa403ed67d7026c99c5b4dba5f3cc4
shlroland <shlroland1995@gmail.com>
2021-05-28 13:34:11 +0800

    fix: 正确启动server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
+  await app.listen(process.env.port || 8023,()=>{
+    console.log(' Rlog Server is listening on port 8023.')
+  });
 }
 bootstrap();
