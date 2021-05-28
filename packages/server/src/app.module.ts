e21d08df601edaa3a6d2215cd0ac89af687e6a1b
shlroland <shlroland1995@gmail.com>
2021-05-28 13:52:48 +0800

    feat(server): 添加graphql

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
