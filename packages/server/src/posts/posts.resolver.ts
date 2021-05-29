84e60603ecb639719676099c60a4bfdff2f8ae35
shlroland <shlroland1995@gmail.com>
2021-05-29 13:54:32 +0800

    feat(server): 初步连接mongo

--- packages/server/src/posts/posts.resolver.ts
+++ packages/server/src/posts/posts.resolver.ts
@@ -1,13 +1,22 @@
-import { Args, Resolver, ID, Query } from '@nestjs/graphql'
+import { Args, Resolver, ID, Query, Mutation } from '@nestjs/graphql'
+import { CreatePostInput } from './dtos/create-post.input'
 import { PostItemModel } from './models/post.model'
+import { PostsService } from './posts.service'
 
 @Resolver()
 export class PostsResolver {
+  constructor(private readonly postsService: PostsService) {}
+
   @Query(() => PostItemModel)
   public async getPostById(@Args({ name: 'id', type: () => ID }) id: string) {
     return {
       id,
       content: '12333',
     }
+  }
+
+  @Mutation(() => PostItemModel)
+  public async createPost(@Args('input') input: CreatePostInput) {
+    return this.postsService.create(input)
   }
 }
