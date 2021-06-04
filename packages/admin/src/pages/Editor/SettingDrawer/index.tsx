bce5e0eadc45d4068b9c23092ebc94ac9e87d579
shlroland <shlroland1995@gmail.com>
2021-06-04 19:03:50 +0800

    feat(admin): 添加设置表单

--- packages/admin/src/pages/Editor/SettingDrawer/index.tsx
+++ packages/admin/src/pages/Editor/SettingDrawer/index.tsx
@@ -1,9 +1,9 @@
 import { SettingTwoTone } from '@ant-design/icons';
 import ProForm, {
   DrawerForm,
-  ProFormDateRangePicker,
   ProFormSelect,
-  ProFormText,
+  ProFormSwitch,
+  ProFormTextArea,
 } from '@ant-design/pro-form';
 import { Button, message } from 'antd';
 import type { FC } from 'react';
@@ -24,7 +24,7 @@ const SettingDrawer: FC = () => {
       name: string;
       company: string;
     }>
-      title="新建表单"
+      title="文章设置"
       formRef={formRef}
       trigger={<Button icon={<SettingTwoTone />}>设置</Button>}
       drawerProps={{
@@ -39,47 +39,36 @@ const SettingDrawer: FC = () => {
         return true;
       }}
     >
+      <ProFormTextArea name="excerpt" label="文章摘要" />
       <ProForm.Group>
-        <ProFormText
-          name="name"
-          width="md"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
-        />
-        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
+        <ProFormSwitch width="md" name="isRecommended" label="首页推荐" />
+        <ProFormSwitch width="md" name="isCommentable" label="开启评论" />
       </ProForm.Group>
-      <ProForm.Group>
-        <ProFormText width="md" name="contract" label="合同名称" placeholder="请输入名称" />
-        <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
-      </ProForm.Group>
-      <ProForm.Group>
-        <ProFormSelect
-          options={[
-            {
-              value: 'chapter',
-              label: '盖章后生效',
-            },
-          ]}
-          width="xs"
-          name="useMode"
-          label="合同约定生效方式"
-        />
-        <ProFormSelect
-          width="xs"
-          options={[
-            {
-              value: 'time',
-              label: '履行完终止',
-            },
-          ]}
-          name="unusedMode"
-          label="合同约定失效效方式"
-        />
-      </ProForm.Group>
-      <ProFormText width="sm" name="id" label="主合同编号" />
-      <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
-      <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+
+      <ProFormSelect
+        width="lg"
+        options={[
+          {
+            value: 'chapter',
+            label: '盖章后生效',
+          },
+        ]}
+        name="category"
+        label="分类"
+      />
+      <ProFormSelect
+        width="lg"
+        mode="multiple"
+        options={[
+          {
+            value: 'time',
+            label: '履行完终止',
+          },
+        ]}
+        name="tags"
+        label="标签"
+      />
+      <ProForm.Item name="imageUrl" label="封面图片"></ProForm.Item>
     </DrawerForm>
   );
 };
