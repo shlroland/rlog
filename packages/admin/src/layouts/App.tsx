dd385646195d7eea286cb69880686167a16cc2b2
shlroland <shlroland1995@gmail.com>
2021-07-10 11:50:47 +0800

    feat(admin): 添加未认证守卫

--- packages/admin/src/layouts/App.tsx
+++ packages/admin/src/layouts/App.tsx
@@ -3,30 +3,50 @@ import { createElement } from 'react'
 // import type { RouteProps } from 'react-router-dom'
 import { Redirect } from 'react-router-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useUserState } from 'src/context/UserContext'
 import Login from 'src/pages/login'
 import Layout from './Layout'
 import type { RouteProps } from './types'
 
 function App() {
+  const { isAuthenticated } = useUserState()
+
   const PrivateRoute: FC<RouteProps> = ({ component, ...rest }) => {
-    return <Route {...rest} render={(props) => createElement(component, props)} />
+    return (
+      <Route
+        {...rest}
+        render={(props) =>
+          isAuthenticated ? (
+            createElement(component, props)
+          ) : (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          )
+        }
+      />
+    )
   }
 
   const PublicRoute: FC<RouteProps> = ({ component, ...rest }) => {
     return (
       <Route
         {...rest}
-        render={
-          (props) =>
-            // isAuthenticated ? (
-            //   <Redirect
-            //     to={{
-            //       pathname: '/',
-            //     }}
-            //   />
-            // ) : (
+        render={(props) =>
+          isAuthenticated ? (
+            <Redirect
+              to={{
+                pathname: '/',
+              }}
+            />
+          ) : (
             createElement(component, props)
-          // )
+          )
         }
       />
     )
@@ -36,9 +56,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
-        <Route exact path="/app" render={() => <Redirect to="/app/tables" />} />
-        <PrivateRoute path="/" component={Layout} />
-        <PublicRoute path="/app" component={Login} />
+        <Route exact path="/app" render={() => <Redirect to="/app/dashboard" />} />
+        <PrivateRoute path="/app" component={Layout} />
+        <PublicRoute path="/login" component={Login} />
       </Switch>
     </Router>
   )
