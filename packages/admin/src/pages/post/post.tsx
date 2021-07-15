855c1465bb77282c5784a344ad70ad7580ed20b6
shlroland <shlroland1995@gmail.com>
2021-07-15 22:55:35 +0800

    feat(admin): 改造datagrid的demo columns

--- packages/admin/src/pages/post/post.tsx
+++ packages/admin/src/pages/post/post.tsx
@@ -1,53 +1,53 @@
-import type { GridColDef, GridValueGetterParams } from '@material-ui/data-grid'
+import type { GridColDef } from '@material-ui/data-grid'
 import { DataGrid } from '@material-ui/data-grid'
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 120 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'articleStatus',
+    headerName: '状态',
     width: 150,
-    editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
-    flex: 1,
-    editable: true,
+    field: 'category',
+    headerName: '分类',
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'tags',
+    headerName: '标签',
     width: 110,
-    editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
+    field: 'updatedAt',
+    headerName: '更新时间',
+    // description: 'This column has a value getter and is not sortable.',
+    // sortable: false,
     width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
+    // valueGetter: (params: GridValueGetterParams) =>
+    //   `${params.getValue(params.id, 'firstName') || ''} ${
+    //     params.getValue(params.id, 'lastName') || ''
+    //   }`,
+  },
+  {
+    headerName: '发布时间',
+    field: 'createdAt',
+    width: 200,
   },
 ]
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    title: 'Snow',
+    articleStatus: 'Jon',
+    category: 35,
+    tags: 'tags',
+    updatedAt: '2020-06-12',
+    createdAt: '2020-07-01',
+  },
 ]
 
-export default function DataGridDemo() {
+const DataGridDemo = () => {
   return (
     <div style={{ height: 400, width: '100%', backgroundColor: '#fff' }}>
       <DataGrid
@@ -60,3 +60,5 @@ export default function DataGridDemo() {
     </div>
   )
 }
+
+export default DataGridDemo
